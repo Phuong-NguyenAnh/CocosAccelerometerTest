ecf1bdadc139af7491903d1d0f3366a03a4e5d13
Phuong <phuongna1908@gmail.com>
2019-12-29 17:17:52 +0700

    * test

--- assets/fps.js
+++ assets/fps.js
@@ -20,6 +20,7 @@ cc.Class({
     onLoad() {
         this.node.x = this.node.y = this.node.z = 0
 
+
         cc.Canvas.instance.node.on(cc.Node.EventType.TOUCH_MOVE, (event) => this._onTouch(event), this)
 
         if (window.DeviceOrientationEvent) {
@@ -28,13 +29,35 @@ cc.Class({
     },
 
     _deviceOrientationHandler(event) {
-        console.log(event)
-        let euler = cc.v3(event.beta, event.alpha, 0)
-        let quat = this.cameraNode.quat.fromEuler(euler)
-        quat.mul(cc.quat(-Math.sqrt(0.5), 0 ,0, Math.sqrt(0.5)), quat) // - PI/2 around the x-axis
-        this.cameraNode.quat = quat
-        this.debug.string = `${parseInt(event.alpha)}, ${parseInt(event.beta)}, ${parseInt(event.gamma)}\n
-        ${parseInt(this.cameraNode.eulerAngles.x)}, ${parseInt(this.cameraNode.eulerAngles.y)}, ${parseInt(this.cameraNode.eulerAngles.z)}\n`
+        let euler = cc.v3(event.beta -90, event.alpha, event.gamma)
+        let quat = this.node.quat.fromEuler(euler)
+
+        //Calculate Pitch in degrees (-180 to 180)
+        let sinP = 2.0 * (quat.w * quat.x + quat.y * quat.z);
+        let cosP = 1.0 - 2.0 * (quat.x * quat.x + quat.y * quat.y);
+        let pitch = Math.atan2(sinP, cosP) * (180 / Math.PI);
+
+        //Calculate Tilt in degrees (-90 to 90)
+        let sinT = 2.0 * (quat.w * quat.y - quat.z * quat.x);
+        let tilt = Math.asin(sinT) * (180 / Math.PI);
+        if (Math.abs(sinT) >= 1)
+            tilt = Math.sign(Math.PI / 2, sinT) * (180 / Math.PI);
+        else
+            tilt = Math.asin(sinT) * (180 / Math.PI);
+    
+        //Calculate Azimuth in degrees (0 to 360; 0 = North, 90 = East, 180 = South, 270 = West)
+        let sinA = 2.0 * (quat.w * quat.z + quat.x * quat.y);
+        let cosA = 1.0 - 2.0 * (quat.y * quat.y + quat.z * quat.z);
+        let azimuth = Math.atan2(sinA, cosA) * (180 / Math.PI);
+
+        console.log(pitch, tilt, azimuth)
+        this.node.eulerAngles = cc.v3(pitch, tilt, azimuth)
+
+        
+        // quat.mul(cc.quat(-Math.sqrt(0.5), 0 ,0, Math.sqrt(0.5)), quat) // - PI/2 around the x-axis
+        // this.cameraNode.quat = quat
+        // this.debug.string = `${parseInt(event.alpha)}, ${parseInt(event.beta)}, ${parseInt(event.gamma)}\n
+        // ${parseInt(this.cameraNode.eulerAngles.x)}, ${parseInt(this.cameraNode.eulerAngles.y)}, ${parseInt(this.cameraNode.eulerAngles.z)}\n`
     },
 
     _onTouch(event) {
@@ -50,14 +73,6 @@ cc.Class({
 
     },
 
-    update(dt) {
-        if (this.deviceOrientation) {
-            let euler = cc.v3(this.deviceOrientation.beta, this.deviceOrientation.alpha, -this.deviceOrientation.gamma)
-            let quat = this.node.quat.fromEuler(euler)
-            quat.mul(cc.quat(-Math.sqrt(0.5), 0, 0, Math.sqrt(0.5)), quat) // - PI/2 around the x-axis
-            this.node.quat = quat
-            this.debug.string = `${parseInt(event.alpha)}, ${parseInt(event.beta)}, ${parseInt(event.gamma)}\n
-            ${parseInt(this.node.eulerAngles.x)}, ${parseInt(this.node.eulerAngles.y)}, ${parseInt(this.node.eulerAngles.z)}\n`
-        }
-    },
+    // update(dt) {
+    // },
 });
